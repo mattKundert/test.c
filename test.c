cceb87d404d2cd41b2ccbf81917b9e4ccc4df268
Nils Kundert <nilskundert@MacBook-Pro-de-Nils.local>
2022-04-05 00:18:55 +0200

    Initial commit

---
+++ test.c
@@ -0,0 +1,12 @@
+//
+// Created by Nils Kundert on 24.03.22.
+//
+#include <stdio.h>
+int main(){
+    int number = 10;
+    int *ptr = 742983;
+    for (int i=1;i<2;i++){
+        printf("%d\n", ptr);
+    }
+    return 0;
+}
